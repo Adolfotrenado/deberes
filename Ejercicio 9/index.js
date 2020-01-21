256423da7a9c865ffc7c53d0cf0aa0cb95ee6d29
Adolfo Trenado <adotrenado@gmail.com>
2020-01-21 17:47:58 +0100

    Ejercicio 9 Hecho

---
+++ Ejercicio 9/index.js
@@ -0,0 +1,9 @@
+function margaritaAmorosa(numero){
+if(numero % 2 === 0){
+    result = "Me quiere";
+} else {
+    result = "Vas a morir solo";
+}
+return result;
+}
+document.write(margaritaAmorosa(1))
